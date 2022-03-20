964ffff0d62c823369da7a70b83ed745ee3b2ce7
Rohit Dimri <rohitdimri@192.168.1.3>
2022-03-21 01:27:49 +0530

    Added play again feature

--- Wordle/Program.cs
+++ Wordle/Program.cs
@@ -6,8 +6,15 @@ namespace Wordle
     {
         static void Main(string[] args)
         {
-            wordle wordle = new wordle();
-            wordle.start();
+            string input = "Y";
+            while (input == "Y")
+            {
+                Console.Clear();
+                wordle wordle = new wordle();
+                wordle.start();
+                Console.WriteLine("Do you wanna play again?(Y/N)");
+                input = Console.ReadLine().ToUpperInvariant();
+            }
         }
     }
 }
